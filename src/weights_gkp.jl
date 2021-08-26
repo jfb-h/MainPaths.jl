82a6d736efa53cc0c05b7afc440b209856a3fc1f
Jakob Hoffmann <jfb-hoffmann@hotmail.de>
2021-08-26 22:56:49 +0200

    renamed files

---
+++ src/weights_gkp.jl
@@ -0,0 +1,116 @@
+
+function _gkp_global(g::AbstractGraph{T}) where T <: Integer
+    n = nv(g)
+    sinks = get_sinks(g)
+    sources = get_sources(g)
+    source_idx = falses(n)
+    source_idx[sources] .= true    
+    visited = falses(n)
+    visited[sinks] .= true
+
+    w = 1 ./ indegree(g)
+    od = outdegree(g)
+    kp = zeros(n)
+    kp[sinks] .= 1.0
+
+    layer = sinks
+    while length(layer) > 0
+        new = T[]
+        
+        for v in layer
+            inb = inneighbors(g, v)
+
+            for nb in inb
+                kp[nb] += w[v] * kp[v]
+                od[nb] -= 1
+
+                if !source_idx[nb] && od[nb] == 0
+                    if !visited[nb] 
+                        push!(new, nb)
+                        visited[nb] = true
+                    end
+                end
+            end 
+        end
+        layer = new
+    end
+
+    kp[sinks] .= 0
+    return kp
+end 
+
+
+function _gkp_local(g::AbstractGraph{T}) where T <: Integer
+    n = nv(g)
+    sinks = get_sinks(g)
+    sources = get_sources(g)
+    source_idx = falses(n)
+    source_idx[sources] .= true    
+    visited = falses(n)
+    visited[sinks] .= true
+    l = zeros(Int, n)
+    l[sinks] .= 1
+
+    w = 1 ./ indegree(g)
+    od = outdegree(g)
+    kp = zeros(n)
+    kp[sinks] .= 1.0
+
+    layer = sinks; lnum = 1
+    while length(layer) > 0
+        new = T[]
+        
+        for v in layer
+            inb = inneighbors(g, v)
+            l[v] = lnum
+
+            for nb in inb
+                kp[nb] += w[v] * kp[v]
+                od[nb] -= 1
+
+                if !source_idx[nb] && od[nb] == 0
+                    if !visited[nb] 
+                        push!(new, nb)
+                        visited[nb] = true
+                    end
+                end
+            end 
+        end
+        layer = new; lnum += 1
+    end
+
+    l[sources] .= lnum
+    kp[sinks] .= 0.0
+    return kp, l
+
+end 
+
+"""
+    weights_gkp(g, normalize=:none)
+
+Compute the genetic knowledge persistence scores for all nodes in `g`. 
+    
+This is a modified version of the method proposed in Martinelli & Nomaler (2014) 
+where citations from future patents to patents before the curent layer are not ignored
+in the persistence computation.
+"""
+function weights_gkp(g::AbstractGraph{T}; normalize::Symbol=:none) where T <: Integer
+    if normalize == :global 
+        res = _gkp_global(g)
+        return res ./ maximum(res)
+    elseif normalize == :local
+        res, layers = _gkp_local(g)
+        for l in unique(layers)
+            res[layers .== l] .= res[layers .== l] ./ maximum(res[layers .== l])
+        end
+        res[isnan.(res)] .= 0.0
+        return res
+    elseif normalize == :none
+        return _gkp_global(g)
+    else
+        throw(ArgumentError("Viable options for normalize are :global, :local or :none."))
+    end
+end
+
+
+    
---
+++ src/weights_spc.jl
@@ -0,0 +1,81 @@
+
+get_sources(g::AbstractGraph{T}) where T <: Integer = vertices(g)[indegree(g) .== 0]
+get_sinks(g::AbstractGraph{T})  where T <: Integer = vertices(g)[outdegree(g) .== 0]
+
+function add_source_target!(g::AbstractGraph{T}) where T <: Integer
+    sinks = get_sinks(g)
+    sources = get_sources(g)
+
+    add_vertex!(g)
+    for source in sources
+        add_edge!(g, vertices(g)[end], source)
+    end
+
+    add_vertex!(g)
+    for sink in sinks
+        add_edge!(g, sink, vertices(g)[end])
+    end
+end
+
+function N⁻(g::LightGraphs.SimpleDiGraph{T}, vseqt::Vector{T}) where T <: Integer
+    nV = length(vseqt)
+    val = zeros(nV)
+    val[vseqt[1]] = 1.0
+    for v in vseqt
+        nb = outneighbors(g, v)
+        val[nb] .+= val[v]
+    end
+    return val
+end
+
+function N⁺(g::LightGraphs.SimpleDiGraph{T}, vseqt::Vector{T}) where T <: Integer
+    nV = length(vseqt)
+    val = zeros(nV)
+    val[vseqt[nV]] = 1.0
+    for v in reverse(vseqt)
+        nb = inneighbors(g, v)
+        val[nb] .+= val[v]
+    end
+    return val
+end
+
+function weights_matrix(g::AbstractGraph{T}, weights::AbstractVector{U}) where T <: Integer where U <: Real
+    senders = [src(e) for e in edges(g)]
+    receivers = [dst(e) for e in edges(g)]
+    return sparse(senders, receivers, weights)
+end
+
+"""
+    weights_spc(g, normalize=false)
+
+Compute the Search Path Count (SPC) weights for graph `g` as described in Batagelj (2003).
+The function returns a vector with edgeweights, a vector with vertex weights and the total
+flow. When `normalize` is set to `true`, edge weigths and vertex weights are divided by
+total flow.
+"""
+function weights_spc(g::AbstractGraph{T}; normalize = false) where T <: Integer
+    g = LightGraphs.SimpleDiGraph(g)
+    add_source_target!(g)
+    vseqt = LightGraphs.topological_sort_by_dfs(g)
+
+    st = vseqt[[1, end]]
+    N_m = N⁻(g, vseqt)
+    N_p = N⁺(g, vseqt)
+    tf = N_p[st[1]]
+    idx = [!(v in st) for v in vertices(g)]
+
+    rem_vertices!(g, st)
+    N_m = N_m[idx]
+    N_p = N_p[idx]
+    vw = N_m .* N_p
+    ew = zeros(ne(g))
+
+    for (i, e) in enumerate(edges(g))
+        ew[i] = N_m[src(e)] * N_p[dst(e)]
+    end
+
+    normalize && return ew ./ tf, vw ./ tf, tf
+    
+    return weights_matrix(g, ew), vw, tf
+end
+
